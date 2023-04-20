fa866f30dd6705e69d17770aff387aaeb0c88141
nandini-malviya <79633387+nandini-malviya@users.noreply.github.com>
2023-04-20 20:52:15 +0530

    Add files via upload

---
+++ conditional.java
@@ -0,0 +1,10 @@
+class conditional {
+    public static void main(String args[]) {
+        
+        int age=21;
+        if (age>=18)
+        System.out.println("eligible for marriage");
+        else
+        System.out.println(" Not eligible for marriage");
+    }
+}
---
+++ manual_casting.java
@@ -0,0 +1,9 @@
+  class manual_casting
+  {
+    public static void main(String args[]){
+
+    double myDouble=54.45d;
+    int myInt= (int) myDouble;
+    System.out.println(myInt);
+    }
+}
